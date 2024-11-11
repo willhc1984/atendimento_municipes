b96e7041b1355938db41f8e1a2f6bb967a837523
will henrique <willhc1984@gmail.com>
2024-11-11 14:54:34 -0300

    Config do Spatie/Laravel Permissison

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
